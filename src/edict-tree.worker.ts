df62e42a0d918f70803d8761864c6bcd33a481cd
blakeoxx <blakeoxx@netscape.net>
2021-07-25 01:06:53 -0500

    Fixed circular references causing edict tree worker to infinitely loop, Added check for worker context before worker setup

--- src/edict-tree.worker.ts
+++ src/edict-tree.worker.ts
@@ -3,16 +3,10 @@ import { Observable, Subject } from 'rxjs';
 import { EdictCollectionComponent } from './app/components/edict-collection.component';
 import { Edict } from './app/data-models/edict';
 import { ExpressionType } from './app/data-models/expression-type';
-import * as domino from 'domino';
 import { deserialize, object, serializable, serialize } from 'serializr';
 
-const FakeWindow: Window = domino.createWindow('<html></html>', 'https://www.subnetroot.com');
-const FakeDocument: Document = FakeWindow.document;
-
-const $: JQueryStatic = require('jquery')(FakeWindow) as JQueryStatic;
-
 class EdictTreeWorkerProcessor {
-    private queue: {edict: Edict | undefined, view: JQuery}[];
+    private queue: {edict: Edict | undefined, view: JQuery, ancestors: string[]}[];
     private completionCount: number;
     private readonly resultTreeView: JQuery;
     private readonly edictCollection: EdictCollectionComponent;
@@ -27,14 +21,23 @@ class EdictTreeWorkerProcessor {
     }
 
     process() {
-        this.queue.push({edict: this.edictCollection.getHeadEntity(), view: this.resultTreeView});
-        this.queue.push({edict: this.edictCollection.getHeadConstraint(), view: this.resultTreeView});
+        this.queue.push({
+            edict: this.edictCollection.getHeadEntity(),
+            view: this.resultTreeView,
+            ancestors: []
+        });
+        this.queue.push({
+            edict: this.edictCollection.getHeadConstraint(),
+            view: this.resultTreeView,
+            ancestors: []
+        });
+
         this.sendProgressMessage();
         const progressUpdater = setInterval(() => { this.sendProgressMessage(); }, 250);
         while (this.queue.length > 0) {
             type queueItemType = typeof EdictTreeWorkerProcessor.prototype.queue[number];
-            const {edict, view} = this.queue.shift() as queueItemType;
-            this.buildEdictTree(edict, view);
+            const {edict, view, ancestors} = this.queue.shift() as queueItemType;
+            this.buildEdictTree(edict, view, ancestors);
             this.completionCount++;
         }
         clearInterval(progressUpdater);
@@ -59,40 +62,63 @@ class EdictTreeWorkerProcessor {
         } as EdictTreeWorkerResponse);
     }
 
-    buildEdictTree(currentEdict: Edict | undefined, parentElem: JQuery) {
+    buildEdictTree(currentEdict: Edict | undefined, parentElem: JQuery, ancestorIDs: string[]) {
         if (currentEdict === undefined)
         {
             parentElem.append($('<div>(undefined edict)</div>'));
             return;
         }
 
-        let label = currentEdict.getID();
+        const thisEdictID = currentEdict.getID();
+
+        // Don't rebuild trees for previously seen edicts. These circular references cause infinite loops
+        const isOwnAncestor = ancestorIDs.includes(thisEdictID);
+        ancestorIDs.push(thisEdictID);
+
+        let label = thisEdictID;
         if (currentEdict === this.edictCollection.getHeadEntity()) label += ' (head entity)';
         else if (currentEdict === this.edictCollection.getHeadConstraint()) label += ' (head constraint)';
+        else if (isOwnAncestor) label += ' (circular)';
 
         // Create the display item without children, in case we need it
         const displayItemWithoutChildren: JQuery = $('<div class="inspectable associative"></div>')
             .text(label)
-            .attr('data-edictid', currentEdict.getID());
+            .attr('data-edictid', thisEdictID);
 
         // Create the display item with children, in case we need it
         const displayItemWithChildren: JQuery = $('<details></details>')
             .append(
                 $('<summary class="inspectable associative"></summary>')
                     .text(label)
-                    .attr('data-edictid', currentEdict.getID())
+                    .attr('data-edictid', thisEdictID)
             );
+
+        // Skip adding children if this is a circular reference
+        if (isOwnAncestor)
+        {
+            console.log('circular reference detected. Ancestors:', ancestorIDs);
+            parentElem.append(displayItemWithoutChildren);
+            return;
+        }
 
         let hasChildren = false;
         currentEdict.getExpressions().forEach((expr) => {
             if (expr.getLeftType() === ExpressionType.TABLE)
             {
-                this.queue.push({edict: this.edictCollection.getEdict(expr.getLeftValue()), view: displayItemWithChildren});
+                this.queue.push({
+                    edict: this.edictCollection.getEdict(expr.getLeftValue()),
+                    view: displayItemWithChildren,
+                    ancestors: [...ancestorIDs]
+                });
                 hasChildren = true;
             }
             if (expr.getRightType() === ExpressionType.TABLE)
             {
-                this.queue.push({edict: this.edictCollection.getEdict(expr.getRightValue()), view: displayItemWithChildren});
+                this.queue.push({
+                    edict: this.edictCollection.getEdict(expr.getRightValue()),
+                    view: displayItemWithChildren,
+                    ancestors: [...ancestorIDs]
+                });
                 hasChildren = true;
             }
         });
@@ -110,12 +136,19 @@ class EdictTreeWorkerProcessor {
 // Web worker setup
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const workerContext: Worker = self as any;
+if (typeof WorkerGlobalScope !== 'undefined' && workerContext instanceof WorkerGlobalScope) {
+    // Create a window and JQuery instance since we don't have those in the worker
+    const FakeWindow = require('domino').createWindow('<html></html>', 'https://www.subnetroot.com') as Window;
+    // @ts-ignore
+    workerContext.$ = require('jquery')(FakeWindow) as JQueryStatic;
 
-workerContext.addEventListener('message', (event: MessageEvent<EdictTreeWorkerRequest>) => {
-    const hydratedRequest = deserialize(EdictTreeWorkerRequest, event.data);
-    const worker = new EdictTreeWorkerProcessor(hydratedRequest.edictCollection, workerContext);
-    worker.process();
-});
+    // Listen for incoming messages
+    workerContext.addEventListener('message', (event: MessageEvent<EdictTreeWorkerRequest>) => {
+        const hydratedRequest = deserialize(EdictTreeWorkerRequest, event.data);
+        const worker = new EdictTreeWorkerProcessor(hydratedRequest.edictCollection, workerContext);
+        worker.process();
+    });
+}
 
 
 // Interfacing class for keeping type safety of messages, since we can't apply typings to the messaging API directly
